ead4203fa1973122205e4efe0304059e3d21ccbe
Farbod Ahmadian <farbodahmadian2014@gmail.com>
2025-01-06 22:08:32 +0100

    challenge #wy1: respond to multiple pings

--- app/server.go
+++ app/server.go
@@ -1,11 +1,40 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"net"
 	"os"
 )
+
+func handleConnection(c net.Conn) {
+	defer c.Close()
+
+	reader := bufio.NewReader(c)
+
+	for {
+		buf, err := reader.ReadBytes('\n')
+		if err != nil {
+			if err.Error() == "EOF" {
+				continue
+			}
+			log.Println("Error reading:", err.Error())
+			break
+		}
+
+		log.Println("Received:", string(buf))
+
+		if string(buf) == "PING\r\n" {
+			log.Println("Responding with PONG")
+			_, writeErr := c.Write([]byte("+PONG\r\n"))
+			if writeErr != nil {
+				log.Println("Error writing:", writeErr.Error())
+				break
+			}
+		}
+	}
+}
 
 func main() {
 	log.Println("Starting server on port 6379")
@@ -15,11 +44,15 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	c, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
 
-	c.Write([]byte("+PONG\r\n"))
+	for {
+		c, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		log.Println("Accepted connection", c.RemoteAddr())
+		go handleConnection(c)
+	}
 }
