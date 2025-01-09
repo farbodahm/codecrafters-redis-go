f484eed81a936ce549333f6540bb89927ee516dc
Farbod Ahmadian <farbodahmadian2014@gmail.com>
2025-01-09 23:17:58 +0100

    initiate RESP parser

--- app/server.go
+++ app/server.go
@@ -2,11 +2,69 @@ package main
 
 import (
 	"bufio"
+	"bytes"
+	"errors"
 	"fmt"
 	"log"
 	"net"
 	"os"
+	"strconv"
 )
+
+var RESPDelimiter = "\r\n"
+
+var ErrUnexpectedStarter = errors.New("expected '*' as starting char of parser")
+var ErrUnexpectedStateOfParser = errors.New("unexpected state of parser")
+var ErrUnexpectedBulkStringStarter = errors.New("expected '$' as starting char of bull string")
+
+type RESPParser struct {
+	parsedArgs              []string
+	nextArgLength           int8
+	remainingExpectedTokens int8
+}
+
+func (p *RESPParser) ParseToken(buf []byte) ([]string, bool, error) {
+	s := string(bytes.TrimRight(buf, RESPDelimiter))
+
+	// Start of a new command
+	if p.remainingExpectedTokens == 0 {
+		if s[0] != '*' {
+			return nil, false, ErrUnexpectedStarter
+		}
+
+		i, err := strconv.Atoi(s[1:])
+		if err != nil {
+			return nil, false, err
+		}
+		p.remainingExpectedTokens = int8(i)
+		return nil, false, nil
+	}
+	// Parsing the length of the next argument
+	if p.nextArgLength == 0 {
+		if s[0] != '$' {
+			return nil, false, ErrUnexpectedBulkStringStarter
+		}
+
+		i, err := strconv.Atoi(s[1:])
+		if err != nil {
+			return nil, false, err
+		}
+		p.nextArgLength = int8(i)
+		return nil, false, nil
+	}
+	// Parsing the next argument
+	if p.nextArgLength > 0 {
+		p.parsedArgs = append(p.parsedArgs, s)
+		p.nextArgLength = 0
+		p.remainingExpectedTokens--
+	}
+	// If we have fully parsed a command
+	if p.remainingExpectedTokens == 0 {
+		return p.parsedArgs, true, nil
+	}
+
+	return p.parsedArgs, false, ErrUnexpectedStateOfParser
+}
 
 func handleConnection(c net.Conn) {
 	defer c.Close()
